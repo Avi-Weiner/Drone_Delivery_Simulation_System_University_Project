147131c75066a4acbbeaf1f4e11843135d79c326
Avi-Weiner <aweiner@g.jct.ac.il>
2021-10-10 12:18:45 +0300

    Initial code plus welcome functions.

---
+++ dotNet5782_9349_0796/Targil0/Program0796.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0796();
+            Welcome9349();
+            Console.ReadKey();
+        }
+        static partial void Welcome9349();
+
+        private static void Welcome0796()
+        {
+            Console.WriteLine("Enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+        
+    }
+}
