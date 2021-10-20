8ac56c74457cd82994c7f5deea3e0b94c53cc516
Avi-Weiner <aweiner@g.jct.ac.il>
2021-10-20 14:58:01 +0300

    printMainMenu() completed and Main() updated as well.

--- dotNet5782_9349_0796/ConsoleUI/Program.cs
+++ dotNet5782_9349_0796/ConsoleUI/Program.cs
@@ -4,9 +4,53 @@ namespace ConsoleUI
 {
     class Program
     {
+        /// <summary>
+        /// Prints main menu
+        /// </summary>
+        static void printMainMenu()
+        {
+            Console.WriteLine("1. Adding options: " +
+                "\n - adding base station to the stations list" +
+                "\n - adding a drone to the existing drones list" +
+                "\n - adding a new customer to the customers list" +
+                "\n - receiving a package to deliver");
+            Console.WriteLine("2.Updating options" +
+                "\n - assigning a package to a drone" +
+                "\n - collecting a package by a drone" +
+                "\n - providing a package to a customer" +
+                "\n - sending a drone to a charge in a base station" +
+                "\n   - by changing the drone’s status and adding a record(instance) of" +
+                "\n     a drone battery charger entity" +
+                "\n   - the station is selected by the user in the main menu(It is" +
+                "\n     recommended to provide a list of stations to the user)" +
+                "\n - releasing a drone from charging in a base station");
+            Console.WriteLine("3. Display options (all chosen by a number):" +
+                "\n - Display a base station" +
+                "\n - Display a drone" +
+                "\n - Display a customer" +
+                "\n - Display a package");
+            Console.WriteLine("4. List display options" +
+                "\n - Displaying base stations list" +
+                "\n - Displaying drones list" +
+                "\n - Displaying customers list" +
+                "\n - Displaying packages list" +
+                "\n - Displaying packages not assigned yet to a drone" +
+                "\n - Displaying base stations with unoccupied charging stations");
+            Console.WriteLine("5. Exit");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int option = 0;
+      
+            while (option != 5) //Option = 5 for exit
+            {
+                printMainMenu();
+                Console.WriteLine("Enter number of option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+
         }
     }
 }
+
--- dotNet5782_9349_0796/DAL/Class1.cs
+++ dotNet5782_9349_0796/DAL/Class1.cs
@@ -9,5 +9,22 @@ namespace IDAL
         {
 
         }
+
+        /// <summary>
+        /// Example enum to show where to put enums as defined in exercise 1.
+        /// </summary>
+        enum example
+        {
+
+        }
+    }
+
+}
+
+namespace DalObject
+{
+    public class DataSource
+    {
+
     }
 }
