862bbd0395ac4b03aa35a8228136e16636a87d16
cfish613 <cfishman@g.jct.ac.il>
2021-10-20 20:04:12 +0300

    not submitting this

--- dotNet5782_9349_0796/DAL/Class1.cs
+++ dotNet5782_9349_0796/DAL/Class1.cs
@@ -6,8 +6,23 @@ namespace IDAL
     namespace DO
     {
         public class Class1
+        { }
+        public struct Customer
+        {
+             
+        }
+
+    }
+
+
+    namespace DalObject
+    {
+        public class DataSource
         {
 
         }
     }
 }
+
+
+
