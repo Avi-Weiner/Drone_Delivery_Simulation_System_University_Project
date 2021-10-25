a4a29bf07e52a2a9ce02f29030cd1f9a25fca345
Avi-Weiner <aweiner@g.jct.ac.il>
2021-10-25 15:04:37 +0300

    some corrections

--- dotNet5782_9349_0796/DAL/Station.cs
+++ dotNet5782_9349_0796/DAL/Station.cs
@@ -2,15 +2,16 @@
 
 namespace IDAL
 {
-    namesapce DO
+    namespace DO
     {
         public struct Station
-    {
-        public int Id { get; set; }
-        public int Name { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public int ChargeSlots { get; set; }
+        {
+            public int Id { get; set; }
+            public int Name { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+            public int ChargeSlots { get; set; }
+        }
     }
     
 }
