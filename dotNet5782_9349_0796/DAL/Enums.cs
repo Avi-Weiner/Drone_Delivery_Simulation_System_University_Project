9af3cb4bdf224375732881ca1f847054bf3d1926
cfish68 <79981544+cfish68@users.noreply.github.com>
2021-10-25 15:53:25 +0300

    changed enums to toher location

---
+++ dotNet5782_9349_0796/DAL/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public enum WeightCategory { light, medium, heavy }
+        public enum DroneStatus { free, maintenance, delivery }
+    }
+}
