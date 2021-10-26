5d21310ce844ddbdb4bc72422475c20d27cee89d
cfish68 <79981544+cfish68@users.noreply.github.com>
2021-10-26 10:45:11 +0300

    added DalObjects

---
+++ dotNet5782_9349_0796/DAL/DalObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DalObject
+    {
+        public class dataSource
+        {
+            static IDAL.DO.Drone[] drones = new IDAL.DO.Drone[10];
+            static IDAL.DO.Station[] Stations = new IDAL.DO.Station[5];
+            static IDAL.DO.Customer[] Customers = new IDAL.DO.Customer[100];
+            static IDAL.DO.Parcel[] Parcels = new IDAL.DO.Parcel[1000];
+            
+
+            class Config
+            {
+                
+            }
+        }
+    }
+}
