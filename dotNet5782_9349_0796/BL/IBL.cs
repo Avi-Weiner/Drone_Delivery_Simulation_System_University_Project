c5c27aaa0b6243eaae1f8b084a2dbc63bd3a2efa
cfish68 <79981544+cfish68@users.noreply.github.com>
2021-11-18 23:26:33 +0200

    Syncying for Syincing

--- dotNet5782_9349_0796/BL/IBL.cs
+++ dotNet5782_9349_0796/BL/IBL.cs
@@ -5,9 +5,9 @@ namespace BL
     public interface IBL
     {
         void AddBaseStation(int Id, string name, double location, int slots);
-        void AddDrone(int manufactureId, string model, /*enum wieght*/ int chargingStation);
+        void AddDrone(int manufactureId, string model, IDAL.DO.WeightCategory Weight, int chargingStation);
         void AddCustomer(int CustomerId, string name, string phone, double Location);
-        void AddPackage(int CustomerId, int ReceiverId /*weight, Priority*/ );
+        void AddPackage(int CustomerId, int ReceiverId, IDAL.DO.WeightCategory Weight, IDAL.DO.Priority Priority );
         void UpdateDrone(int Id, string Model);
         void UpdateStation(string StationName = "", int chargingStation = 0);
         void UpdateCustomer(int Id, string Name = "", string Phone = "");
