963e058fd7c66dc93efbc303cddb77890a3c55f2
cfish68 <79981544+cfish68@users.noreply.github.com>
2021-11-18 22:49:19 +0200

    added IBL interface class.

---
+++ dotNet5782_9349_0796/BL/IBL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BL
+{
+    public interface IBL
+    {
+        void AddBaseStation(int Id, string name, double location, int slots);
+        void AddDrone(int manufactureId, string model, /*enum wieght*/ int chargingStation);
+         void AddCustomer();
+        void AddPackage();
+        void updateDrone(int Id, string Model);
+    }
+}
