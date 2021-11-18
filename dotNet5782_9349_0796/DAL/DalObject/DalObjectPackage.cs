3385b9d4912ff93b52c2a682af33bbf69bb9f0cd
Avi-W <avi.weiner12@gmail.com>
2021-11-19 00:09:46 +0200

    updating DalObject input output and parameters

--- dotNet5782_9349_0796/DAL/DalObject/DalObjectPackage.cs
+++ dotNet5782_9349_0796/DAL/DalObject/DalObjectPackage.cs
@@ -25,21 +25,21 @@ namespace DalObject
         /// <summary>
         /// receiving a package to deliver
         /// </summary>
-        public static void AddPackage()
+        public static void AddPackage(int InputSender, int InputReceiver, IDAL.DO.WeightCategory InputWeight)
         {
             try
             {
-                Console.WriteLine("Enter Sender ID: ");
-                int InputSender = Convert.ToInt32(Console.ReadLine()); //Does not check if Customer exists as Exercise 1 says not to
-                Console.WriteLine("Enter Receiver ID: ");
-                int InputReciever = Convert.ToInt32(Console.ReadLine());//Does not check if Customer exists as Exercise 1 says not to
-                Console.WriteLine("Enter Weight Catagory ('light', 'medium', 'heavy'): ");
-                string weightString = Console.ReadLine();
-                //Check if valid input
-                if (weightString != "light" && weightString != "medium" && weightString != "heavy")
-                    throw new IDAL.DO.MessageException("Error: Invalid weight.");
+                //Console.WriteLine("Enter Sender ID: ");
+                //int InputSender = Convert.ToInt32(Console.ReadLine()); //Does not check if Customer exists as Exercise 1 says not to
+                //Console.WriteLine("Enter Receiver ID: ");
+                //int InputReciever = Convert.ToInt32(Console.ReadLine());//Does not check if Customer exists as Exercise 1 says not to
+                //Console.WriteLine("Enter Weight Catagory ('light', 'medium', 'heavy'): ");
+                //string weightString = Console.ReadLine();
+                ////Check if valid input
+                //if (weightString != "light" && weightString != "medium" && weightString != "heavy")
+                //    throw new IDAL.DO.MessageException("Error: Invalid weight.");
 
-                IDAL.DO.WeightCategory InputWeight = (IDAL.DO.WeightCategory)Enum.Parse(typeof(IDAL.DO.WeightCategory), weightString);
+                //IDAL.DO.WeightCategory InputWeight = (IDAL.DO.WeightCategory)Enum.Parse(typeof(IDAL.DO.WeightCategory), weightString);
 
                 Console.WriteLine("Enter Priority ('regular', 'fast', 'emergency'): ");
                 string priorityString = Console.ReadLine();
@@ -52,7 +52,7 @@ namespace DalObject
                 {
                     Id = DataSource.GetNextUniqueID(),
                     SenderId = InputSender,
-                    ReceiverId = InputReciever,
+                    ReceiverId = InputReceiver,
                     Weight = InputWeight,
                     Priority = InputPriority,
                     Requested = DateTime.Now,
@@ -77,19 +77,19 @@ namespace DalObject
         /// <param name="DroneId"></param>
         public static void AssignDroneToPackage(int PackageId, int DroneId)
         {
-            try
-            {
+            //try
+            //{
                 //Checks if drone Id is valid
                 GetDrone(DroneId);
                 IDAL.DO.Package P = DataSource.PackageList.Find(x => x.Id == PackageId);
 
                 P.DroneId = DroneId;
                 P.Scheduled = DateTime.Now;
-            }
-            catch (IDAL.DO.MessageException e)
-            {
-                Console.WriteLine(e);
-            }
+            //}
+            //catch (IDAL.DO.MessageException e)
+            //{
+            //    Console.WriteLine(e);
+            //}
         }
 
         /// <summary>
@@ -98,8 +98,8 @@ namespace DalObject
         /// <param name="PackageId"></param>
         public static void PackageDropOff(int PackageId)
         {
-            try
-            {
+            //try
+            //{
                 //hopefully will catch if packageId is not found
                 IDAL.DO.Package P = DataSource.PackageList.Find(x => x.Id == PackageId);
                 P.Delivered = DateTime.Now;
@@ -108,70 +108,70 @@ namespace DalObject
                 //DataSource.PackageList[p].Delivered = DateTime.Now;
                 //int DroneId = DataSource.PackageList[p].DroneId;
                 //int i = GetDrone(DroneId);
-            }
-            catch (IDAL.DO.MessageException e)
-            {
-                Console.WriteLine(e);
-            }
+            //}
+            //catch (IDAL.DO.MessageException e)
+            //{
+            //    Console.WriteLine(e);
+            //}
 
         }
+        //-------------------------------------------------------------Below this will be put in the main-------------------
+        ///// <summary>
+        ///// Prints a package given its' index in PackageList
+        ///// </summary>
+        ///// <param name="i"></param>
+        //public static void PrintPackage(IDAL.DO.Package p)
+        //{
+        //    Console.WriteLine("\nPackage ID: " + p.Id
+        //        + "\nSender ID: " + p.SenderId
+        //        + "\nReceiver ID: " + p.ReceiverId
+        //        + "\nWeight: " + p.ToString()
+        //        + "\nPriority: " + p.Priority.ToString()
+        //        + "\nRequested Date: " + p.Requested.ToString()
+        //        + "\nDrone ID: " + p.DroneId
+        //        + "\nScheduled: " + p.Scheduled.ToString()
+        //        + "\nPick up Date: " + p.PickedUp.ToString()
+        //        + "\nDelivery Date: " + p.Delivered.ToString());
+        //}
 
-        /// <summary>
-        /// Prints a package given its' index in PackageList
-        /// </summary>
-        /// <param name="i"></param>
-        public static void PrintPackage(IDAL.DO.Package p)
-        {
-            Console.WriteLine("\nPackage ID: " + p.Id
-                + "\nSender ID: " + p.SenderId
-                + "\nReceiver ID: " + p.ReceiverId
-                + "\nWeight: " + p.ToString()
-                + "\nPriority: " + p.Priority.ToString()
-                + "\nRequested Date: " + p.Requested.ToString()
-                + "\nDrone ID: " + p.DroneId
-                + "\nScheduled: " + p.Scheduled.ToString()
-                + "\nPick up Date: " + p.PickedUp.ToString()
-                + "\nDelivery Date: " + p.Delivered.ToString());
-        }
+        ///// <summary>
+        ///// Displays the package info of the given package id.
+        ///// </summary>
+        ///// <param name="Id"></param>
+        //public static void DisplayPackage(int Id)
+        //{
+        //    try
+        //    {
+        //        IDAL.DO.Package p = DataSource.PackageList.Find(x => x.Id == Id);
+        //        PrintPackage(p);
+        //    }
+        //    catch (IDAL.DO.MessageException e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+        //}
 
-        /// <summary>
-        /// Displays the package info of the given package id.
-        /// </summary>
-        /// <param name="Id"></param>
-        public static void DisplayPackage(int Id)
-        {
-            try
-            {
-                IDAL.DO.Package p = DataSource.PackageList.Find(x => x.Id == Id);
-                PrintPackage(p);
-            }
-            catch (IDAL.DO.MessageException e)
-            {
-                Console.WriteLine(e);
-            }
-        }
+        ///// <summary>
+        ///// Displays all the Packages in PackageList
+        ///// </summary>
+        //public static void DisplayPackageList()
+        //{
+        //    foreach (IDAL.DO.Package p in DataSource.PackageList)
+        //    {
+        //        PrintPackage(p);
+        //    }
+        //}
 
-        /// <summary>
-        /// Displays all the Packages in PackageList
-        /// </summary>
-        public static void DisplayPackageList()
-        {
-            foreach (IDAL.DO.Package p in DataSource.PackageList)
-            {
-                PrintPackage(p);
-            }
-        }
-
-        /// <summary>
-        /// Displays packages that are not yet assigned to a drone
-        /// </summary>
-        public static void DisplayUnassignedPackages()
-        {
-            foreach (IDAL.DO.Package p in DataSource.PackageList)
-            {
-                if (p.DroneId == 0)
-                    PrintPackage(p);
-            }
-        }
+        ///// <summary>
+        ///// Displays packages that are not yet assigned to a drone
+        ///// </summary>
+        //public static void DisplayUnassignedPackages()
+        //{
+        //    foreach (IDAL.DO.Package p in DataSource.PackageList)
+        //    {
+        //        if (p.DroneId == 0)
+        //            PrintPackage(p);
+        //    }
+        //}
     }
 }
--- dotNet5782_9349_0796/DAL/DalObject/DalObjectStation.cs
+++ dotNet5782_9349_0796/DAL/DalObject/DalObjectStation.cs
@@ -23,14 +23,17 @@ namespace DalObject
         /// <summary>
         /// adding base station to the stations list
         /// </summary>
-        public static void AddStation()
+        /// <param name="longitude"></param>
+        /// <param name="latitude"></param>
+        /// <param name="slots"></param>
+        public static void AddStation(double longitude, double latitude, int slots)
         {
-            Console.WriteLine("Enter Longitude: ");
-            double longitude = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter Latitude: ");
-            double latitude = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter number of charge slots: ");
-            int slots = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Longitude: ");
+            //double longitude = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter Latitude: ");
+            //double latitude = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter number of charge slots: ");
+            //int slots = Convert.ToInt32(Console.ReadLine());
 
             //assuming that no maximum amount of sattions
             //add station to the back of the station list
@@ -42,59 +45,59 @@ namespace DalObject
                 ChargeSlots = slots
             });
         }
+        ////-------------------------------------------Below this is printing which will be put in the main-------------------
+        ///// <summary>
+        ///// Prints Station from received station s
+        ///// </summary>
+        ///// <param name="i"></param>
+        //public static void PrintStation(IDAL.DO.Station s)
+        //{
+        //    Console.WriteLine("\nBase Station ID: " + s.Id
+        //       + "\nBase Station  name: " + s.Name
+        //       + "\nBase Station Longitude: " + s.Longitude
+        //       + "\nBase Station Latitude: " + s.Latitude
+        //       + "\nBase Station # of Charging slots: " + s.ChargeSlots);
+        //}
 
-        /// <summary>
-        /// Prints Station from received station s
-        /// </summary>
-        /// <param name="i"></param>
-        public static void PrintStation(IDAL.DO.Station s)
-        {
-            Console.WriteLine("\nBase Station ID: " + s.Id
-               + "\nBase Station  name: " + s.Name
-               + "\nBase Station Longitude: " + s.Longitude
-               + "\nBase Station Latitude: " + s.Latitude
-               + "\nBase Station # of Charging slots: " + s.ChargeSlots);
-        }
+        ///// <summary>
+        ///// Prints the base station from the given ID
+        ///// </summary>
+        ///// <param name="Id"></param>
+        //public static void DisplayBaseStation(int Id)
+        //{
+        //    try
+        //    {
+        //        IDAL.DO.Station s = DataSource.StationList.Find(x => x.Id == Id);
+        //        PrintStation(s);
+        //    }
+        //    catch (IDAL.DO.MessageException e)
+        //    {
+        //        Console.WriteLine(e);
+        //    }
+        //}
 
-        /// <summary>
-        /// Prints the base station from the given ID
-        /// </summary>
-        /// <param name="Id"></param>
-        public static void DisplayBaseStation(int Id)
-        {
-            try
-            {
-                IDAL.DO.Station s = DataSource.StationList.Find(x => x.Id == Id);
-                PrintStation(s);
-            }
-            catch (IDAL.DO.MessageException e)
-            {
-                Console.WriteLine(e);
-            }
-        }
+        ///// <summary>
+        ///// Displays all the stations in StationList.
+        ///// </summary>
+        //public static void DisplayStationList()
+        //{
+        //    foreach (IDAL.DO.Station s in DataSource.StationList)
+        //    {
+        //        PrintStation(s);
+        //    }
 
-        /// <summary>
-        /// Displays all the stations in StationList.
-        /// </summary>
-        public static void DisplayStationList()
-        {
-            foreach (IDAL.DO.Station s in DataSource.StationList)
-            {
-                PrintStation(s);
-            }
+        //}
 
-        }
-
-        /// <summary>
-        /// Displays Stations with unoccupied charging stations
-        /// </summary>
-        public static void DisplayFreeChargingStations()
-        {
-            foreach (IDAL.DO.Station s in DataSource.StationList)
-            {
-                if (s.ChargeSlots > 0)
-                    PrintStation(s);
-            }
-        }
+        ///// <summary>
+        ///// Displays Stations with unoccupied charging stations
+        ///// </summary>
+        //public static void DisplayFreeChargingStations()
+        //{
+        //    foreach (IDAL.DO.Station s in DataSource.StationList)
+        //    {
+        //        if (s.ChargeSlots > 0)
+        //            PrintStation(s);
+        //    }
+        //}
     }
 }
