568193f5546d3bcd332f93732aad716fe48338be
cfish68 <79981544+cfish68@users.noreply.github.com>
2021-11-19 00:19:34 +0200

    commit

--- dotNet5782_9349_0796/BL/BL/BLAdd.cs
+++ dotNet5782_9349_0796/BL/BL/BLAdd.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace BL
 {
+
     public partial class BL : IBL.IBL
     {
-        void AddBaseStation(int Id, string name, double location, int slots)
+        /// <summary>
+        /// Adds Station List to list.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="name"></param>
+        /// <param name="longitude"></param>
+        /// <param name="latitude"></param>
+        /// <param name="slots"></param>
+        void AddBaseStation(int Id, int name, double longitude, double latitude, int slots)
         {
-
+            DalObject.DalObject.AddStation(longitude, latitude, slots);
         }
 
         void AddDrone(int manufactureId, string model, IDAL.DO.WeightCategory Weight, int chargingStation)
         {
-
+            
         }
         void AddCustomer(int CustomerId, string name, string phone, double Longitude, double Latitude)
         {
