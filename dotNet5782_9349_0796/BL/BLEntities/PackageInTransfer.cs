d1a47c468c761b600335bfa56992a95bebd499dd
Avi-W <avi.weiner12@gmail.com>
2021-11-19 12:30:16 +0200

    Final ish entity sync

--- dotNet5782_9349_0796/BL/BLEntities/PackageInTransfer.cs
+++ dotNet5782_9349_0796/BL/BLEntities/PackageInTransfer.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace IBL
 {
     namespace BO
     {
-        class PackageInTransfer
+        public class PackageInTransfer
         {
+            public int ID { get; set; }
+            public IDAL.DO.WeightCategory Weight { get; set; }
+            public IDAL.DO.Priority Priority { get; set; }
+            /// <summary>
+            /// 0 = waiting for delivery, 1 = on delivery
+            /// </summary>
+            public bool DeliveryStatus { get; set; }
+            public CustomerInPackage Sender { get; set; }
+            public CustomerInPackage Receiver { get; set; }
+            public Location CollectLocation { get; set; }
+            public Location DeliveryLocation { get; set; }
+            public int DeliveryDistance { get; set; }
+
         }
     }
 }
