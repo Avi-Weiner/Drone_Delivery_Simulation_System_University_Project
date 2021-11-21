6a0a8929ac226318d7a692ea30372febb0ecfb7f
Avi-W <avi.weiner12@gmail.com>
2021-11-21 12:40:41 +0200

    InputModule, PrintModule, MainModule

---
+++ dotNet5782_9349_0796/ConsoleUI_BL/InputModule.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI_BL
+{
+    partial class Program
+    {
+        /// <summary>
+        /// Adds a base station
+        /// </summary>
+        /// <param name="Bl"></param>
+        static void AddBaseStation(BL.BL Bl)
+        {
+            int name; double longitude, latitude; int availableSlots;
+            Console.WriteLine("Enter: (Name, Longitude, Latitude, Available Slots)\n");
+            name = Convert.ToInt32(Console.ReadLine());
+            longitude = Convert.ToDouble(Console.ReadLine());
+            latitude = Convert.ToDouble(Console.ReadLine());
+            availableSlots = Convert.ToInt32(Console.ReadLine());
+
+            Bl.AddBaseStation(name, longitude, latitude, availableSlots);
+        }
+        /// <summary>
+        /// adds a drone
+        /// </summary>
+        /// <param name="Bl"></param>
+        static void AddDrone(BL.BL Bl)
+        {
+            Console.WriteLine("Enter Model:\n");
+            string Model = Console.ReadLine();
+            Console.WriteLine("Enter WeightCategory(string):\n");
+            IDAL.DO.WeightCategory Weight = (IDAL.DO.WeightCategory)Enum.Parse(typeof(IDAL.DO.WeightCategory), Console.ReadLine());
+            Console.WriteLine("Enter StationId(For Charging):\n");
+            int stationId = Convert.ToInt32(Console.ReadLine());
+            Bl.AddDrone(Model, Weight, stationId);
+        }
+        /// <summary>
+        /// adds a new customer
+        /// </summary>
+        /// <param name="Bl"></param>
+        static void AddNewCustomer(BL.BL Bl)
+        {
+            Console.WriteLine("Enter CustomerId:\n");
+            int CustomerId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter Name:\n");
+            string name = Console.ReadLine();
+            Console.WriteLine(" Enter Phone:\n");
+            string phone = Console.ReadLine();
+            Console.WriteLine(" Enter Longitude:\n");
+            double Longitude = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" Enter Latitude:\n");
+            double Latitude = Convert.ToDouble(Console.ReadLine());
+            Bl.AddCustomer(CustomerId, name, phone, Longitude, Latitude);
+        }
+        /// <summary>
+        /// Adds a new package
+        /// </summary>
+        /// <param name="Bl"></param>
+        static void AddAPakcage(BL.BL Bl)
+        {
+            Console.WriteLine("Enter SenderId:\n");
+            int SenderId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ReceiverId:\n");
+            int RecieiverId = Convert.ToInt32(Console.ReadLine());
+            IDAL.DO.WeightCategory Weight = (IDAL.DO.WeightCategory)Enum.Parse(typeof(IDAL.DO.WeightCategory), Console.ReadLine());
+            IDAL.DO.Priority Priority = (IDAL.DO.Priority)Enum.Parse(typeof(IDAL.DO.Priority), Console.ReadLine());
+            Bl.AddPackage(SenderId, RecieiverId, Weight, Priority);
+        }
+        /// <summary>
+        /// update a drone
+        /// Model could be updated
+        /// </summary>
+        /// <param name="Bl"></param>
+        static void UpdateDrone(BL.BL Bl)
+        {
+            Console.WriteLine("Enter Drone Id:\n");
+            int Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter new Model:\n");
+            string Model = Console.ReadLine();
+            Bl.UpdateDrone(Id, Model);
+        }
+    }
---
+++ dotNet5782_9349_0796/ConsoleUI_BL/MainModule.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace ConsoleUI_BL
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = 0;
+            int InnerOption;
+            BL.BL Bl = new BL.BL();
+            
+            
+            while (option != 6) //Option = 6 for exit
+            {
+                PrintMainMenu();
+                Console.WriteLine("\nEnter number of option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1://adding options
+                        PrintAddingMenu();
+                        Console.WriteLine("\nEnter number of option: ");
+                        InnerOption = Convert.ToInt32(Console.ReadLine());
+                        switch (InnerOption)
+                        {
+                            case 1:
+                                AddBaseStation(Bl);
+                                break;
+                            case 2:
+                                AddDrone(Bl);
+                                break;
+                            case 3:
+                                AddNewCustomer(Bl);
+                                break;
+                            case 4:
+                                AddAPakcage(Bl);
+                                break;
+                        }
+                        break;
+
+                    case 2://updating uptions
+                        PrintUpdatingOptionsMenu();
+                        Console.WriteLine("Enter number of option: ");
+                        InnerOption = Convert.ToInt32(Console.ReadLine());
+                        switch (InnerOption)
+                        {
+                            case 1:
+                                UpdateDrone(Bl);
+                                break;
+                            case 2:
+                                //not sure about the funciton to update Station 
+                                break;
+                            case 3:
+                                //not sure about the function to update Customer the question is how to I make the adding optional?
+                                break;
+
+                        }
+
+                        break;
+
+                    case 3://Drone actions
+                        PrintDroneActionsMenu();
+                        Console.WriteLine("Enter number of option: ");
+                        InnerOption = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter drone Id:\n");
+                        int DroneId = Convert.ToInt32(Console.ReadLine());
+                        switch (InnerOption)
+                        {
+                            case 1:
+                                Bl.SendDroneToCharge(DroneId);
+                                break;
+                            case 2:
+                                Bl.ReleaseDroneFromCharge(DroneId);
+                                break;
+                            case 3:
+                                Bl.DroneCollectsAPackage(DroneId);
+                                break;
+                            case 4:
+                                Bl.DroneDeliversPakcage(DroneId);
+                                break;
+                        }
+
+                        break;
+
+                    case 4://Dispaly option
+
+                        Console.WriteLine("\nEnter number of option: ");
+                        InnerOption = Convert.ToInt32(Console.ReadLine());
+                        switch (InnerOption)
+                        {
+                            //            Console.WriteLine("\n\t1. - Base Station by Id." +
+                            //"\n\t2. - Drone by Id." +
+                            //"\n\t3. - Package by Id." +
+                            //"\n\t4. - Base Station by Id." +
+                            //"\n\t5. - List display option: ");
+                            case 1:
+                                Console.WriteLine("Enter Base Station Id:\n");
+                                int Id = Convert.ToInt32(Console.ReadLine());
+                                //are there any print functions?
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                PrintListDisplayOptions();
+                                Console.WriteLine("Enter number of option:");
+                                InnerOption = Convert.ToInt32(Console.ReadLine());
+                                switch (InnerOption)
+                                {
+                                    case 1:
+
+                                        break;
+                                    case 2:
+                                        break;
+                                    case 3:
+                                        break;
+                                    case 4:
+                                        break;
+                                    case 5:
+                                        break;
+                                    case 6:
+                                        break;
+                                }
+
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
