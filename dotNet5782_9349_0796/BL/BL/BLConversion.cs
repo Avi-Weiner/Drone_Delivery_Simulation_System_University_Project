249faab17bc907992318fda9a4851476a0dadd76
Avi-W <avi.weiner12@gmail.com>
2021-11-21 12:17:19 +0200

    DalToBLStation() done

--- dotNet5782_9349_0796/BL/BL/BLConversion.cs
+++ dotNet5782_9349_0796/BL/BL/BLConversion.cs
@@ -18,8 +18,21 @@ namespace BL
             b.Id = s.Id;
             b.Name = s.Name;
             b.Location = BL.BLObject.MakeLocation(s.Longitude, s.Latitude);
+            b.AvailableChargeSlots = s.ChargeSlots;
 
+            List<IBL.BO.DroneInCharge> chargingDrones = new();
 
+            foreach(IBL.BO.DroneToList d in BLObject.DroneList)
+            {
+                if (d.Location == b.Location && d.DroneStatus == IBL.BO.DroneStatus.maintenance)
+                {
+                    IBL.BO.DroneInCharge droneInCharge = new();
+                    droneInCharge.Id = d.Id;
+                    droneInCharge.BatteryStatus = d.BatteryStatus;
+                    chargingDrones.Add(droneInCharge);
+                }
+            }
+            b.ChargingDroneList = chargingDrones;
 
             return b;
         }
