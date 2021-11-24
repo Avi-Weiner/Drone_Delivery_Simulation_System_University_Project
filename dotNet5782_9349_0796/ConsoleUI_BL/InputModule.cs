8f015face22666b1666121e43a592b7842837a3c
Avi-W <avi.weiner12@gmail.com>
2021-11-24 15:46:38 +0200

    standadised InputeModule in ConsoleUI_BL

--- dotNet5782_9349_0796/ConsoleUI_BL/InputModule.cs
+++ dotNet5782_9349_0796/ConsoleUI_BL/InputModule.cs
@@ -15,10 +15,13 @@ namespace ConsoleUI_BL
         static void AddBaseStation(BL.BL Bl)
         {
             int name; double longitude, latitude; int availableSlots;
-            Console.WriteLine("Enter: (Name, Longitude, Latitude, Available Slots)\n");
+            Console.WriteLine("\nEnter Name:");
             name = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter Longitude: ");
             longitude = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nEnter: Latitude: ");
             latitude = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nEnter Available Slots: ");
             availableSlots = Convert.ToInt32(Console.ReadLine());
 
             Bl.AddBaseStation(name, longitude, latitude, availableSlots);
@@ -29,11 +32,11 @@ namespace ConsoleUI_BL
         /// <param name="Bl"></param>
         static void AddDrone(BL.BL Bl)
         {
-            Console.WriteLine("Enter Model:\n");
+            Console.WriteLine("\nEnter Model: ");
             string Model = Console.ReadLine();
-            Console.WriteLine("Enter WeightCategory(string):\n");
+            Console.WriteLine("\nEnter WeightCategory(string): ");
             IDAL.DO.WeightCategory Weight = (IDAL.DO.WeightCategory)Enum.Parse(typeof(IDAL.DO.WeightCategory), Console.ReadLine());
-            Console.WriteLine("Enter StationId(For Charging):\n");
+            Console.WriteLine("\nEnter StationId(For Charging): ");
             int stationId = Convert.ToInt32(Console.ReadLine());
             Bl.AddDrone(Model, Weight, stationId);
         }
@@ -43,13 +46,13 @@ namespace ConsoleUI_BL
         /// <param name="Bl"></param>
         static void AddNewCustomer(BL.BL Bl)
         {
-            Console.WriteLine(" Enter Name:\n");
+            Console.WriteLine("\nEnter Name: ");
             string name = Console.ReadLine();
-            Console.WriteLine(" Enter Phone:\n");
+            Console.WriteLine("\nEnter Phone: ");
             string phone = Console.ReadLine();
-            Console.WriteLine(" Enter Longitude:\n");
+            Console.WriteLine("\nEnter Longitude: ");
             double Longitude = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(" Enter Latitude:\n");
+            Console.WriteLine("\nEnter Latitude: ");
             double Latitude = Convert.ToDouble(Console.ReadLine());
             Bl.AddCustomer(name, phone, Longitude, Latitude);
         }
@@ -59,9 +62,9 @@ namespace ConsoleUI_BL
         /// <param name="Bl"></param>
         static void AddAPakcage(BL.BL Bl)
         {
-            Console.WriteLine("Enter SenderId:\n");
+            Console.WriteLine("\nEnter SenderId: ");
             int SenderId = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter ReceiverId:\n");
+            Console.WriteLine("\nEnter ReceiverId: ");
             int RecieiverId = Convert.ToInt32(Console.ReadLine());
             IDAL.DO.WeightCategory Weight = (IDAL.DO.WeightCategory)Enum.Parse(typeof(IDAL.DO.WeightCategory), Console.ReadLine());
             IDAL.DO.Priority Priority = (IDAL.DO.Priority)Enum.Parse(typeof(IDAL.DO.Priority), Console.ReadLine());
@@ -74,9 +77,9 @@ namespace ConsoleUI_BL
         /// <param name="Bl"></param>
         static void UpdateDrone(BL.BL Bl)
         {
-            Console.WriteLine("Enter Drone Id:\n");
+            Console.WriteLine("\nEnter Drone Id: ");
             int Id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter new Model:\n");
+            Console.WriteLine("\nEnter new Model: ");
             string Model = Console.ReadLine();
             Bl.UpdateDrone(Id, Model);
         }
@@ -87,22 +90,22 @@ namespace ConsoleUI_BL
         /// <param name="Bl"></param>
         static void UpdateStation(BL.BL Bl)
         {
-            Console.WriteLine("Enter Station ID: ");
+            Console.WriteLine("\nEnter Station ID: ");
             int Id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter new station name or -1 to not change: ");
+            Console.WriteLine("\nEnter new station name or -1 to not change: ");
             int stationName = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter new charge station amount or -1 to not change: ");
+            Console.WriteLine("\nEnter new charge station amount or -1 to not change: ");
             int chargeStations = Convert.ToInt32(Console.ReadLine());
             Bl.UpdateStation(Id, stationName, chargeStations);
         }
 
         static void UpdateCustomer(BL.BL Bl)
         {
-            Console.WriteLine("Enter Customer ID: ");
+            Console.WriteLine("\nEnter Customer ID: ");
             int Id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter new customer name or -1 to not change: ");
+            Console.WriteLine("\nEnter new customer name or -1 to not change: ");
             string Name = Console.ReadLine();
-            Console.WriteLine("Enter new phone number amount or -1 to not change: ");
+            Console.WriteLine("\nEnter new phone number amount or -1 to not change: ");
             string phone = Console.ReadLine();
             Bl.UpdateCustomer(Id, Name, phone);
         }
