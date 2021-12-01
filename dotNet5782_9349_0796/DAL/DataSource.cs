a1a7dc73a9f45730e07d27e0a8f3cf0eb7d145cd
Avi-W <avi.weiner12@gmail.com>
2021-12-01 14:38:27 +0200

    added nextUniqueId++ to packages

--- dotNet5782_9349_0796/DAL/DataSource.cs
+++ dotNet5782_9349_0796/DAL/DataSource.cs
@@ -131,7 +131,8 @@ namespace DalObject
                     Scheduled = (DateTime.Now).AddDays(5),
                     PickedUp = null,
                     Delivered = null
-                }); 
+                });
+                Config.NextUniqueId++;
 
             }
         }
