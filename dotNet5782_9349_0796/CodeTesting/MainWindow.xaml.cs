776d19f336d1e6ceb02edebf5ba8e604bb140209
cfish68 <79981544+cfish68@users.noreply.github.com>
2021-12-02 14:36:31 +0200

    adde code Testing for wpf

--- dotNet5782_9349_0796/CodeTesting/MainWindow.xaml.cs
+++ dotNet5782_9349_0796/CodeTesting/MainWindow.xaml.cs
@@ -13,23 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PL
+namespace CodeTesting
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void DronesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            DroneDisplay DroneDisplayWindow = new DroneDisplay();
-            DroneDisplayWindow.Show();
         }
     }
 }
