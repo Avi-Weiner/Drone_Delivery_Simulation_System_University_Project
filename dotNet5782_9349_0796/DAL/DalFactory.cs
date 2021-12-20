1bb6ed6ed12ea6633a4261b0ae5553b08c06bbae
cfish68 <79981544+cfish68@users.noreply.github.com>
2021-12-20 11:23:35 +0200

    made DalFactory and made DalObject a singleton. next step is to fix it down the line

---
+++ dotNet5782_9349_0796/DAL/DalFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DalFactory
+    {
+        static DalApi.IDAL GetDal(string param)
+        {
+            return DalObject.DalObject.GetDalObject();
+        }
+    }
+    
+}
+
+
--- dotNet5782_9349_0796/DAL/DalObject/DalObject.cs
+++ dotNet5782_9349_0796/DAL/DalObject/DalObject.cs
@@ -11,11 +11,27 @@ namespace DalObject
     public partial class DalObject : DalApi.IDAL
     {
         //creates a DAL object by intializing values accordign to Initialize
-        public DalObject()
+        
+        static DalObject ThisObject = null;
+        private DalObject()
         {
             DataSource.Initialize();//constructor for DalObjects
         }
+        /// <summary>
+        /// if dal was already intialized the object already there will be returned 
+        /// otherwise a new one will be created.
+        /// </summary>
+        /// <returns></returns>
+        public static DalObject GetDalObject()
+        {
+            if(ThisObject == null)
+            {
+                ThisObject = new DalObject();
+            }
 
+            return ThisObject;
+                
+        }
         /// <summary>
         /// Returns an array of the power consumptions { Free, Light, Medium, Heavy, ChargingRate }
         /// </summary>
